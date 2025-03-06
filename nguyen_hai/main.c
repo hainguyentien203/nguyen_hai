b0fbce6316614be284d47a9a8b1281eaf346a93c
hainguyentien203 <hainguyentien203@gmail.com>
2025-03-06 19:49:33 +0700

    tim so lon nhat cua a va b

--- nguyen_hai/main.c
+++ nguyen_hai/main.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
 void main() {
-	printf("hello world \n");
+	float a = 0;
+	float b = 0;
+	printf(" Nhap gia tri cua a: ");
+	scanf_s("%f", &a);
+	printf(" Nhap gia tri cua b: ");
+	scanf_s("%f", &b);
+	float max = a;
+	if (b >= max) {
+		printf("Gia tri lon nhat la b= %f",b);
+	}
+	else printf("Gia tri lon nhat la a = %f",a);
 }
