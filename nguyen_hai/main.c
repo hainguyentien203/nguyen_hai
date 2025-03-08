43c9de171f373d25aab9cbf55de80d2878eff724
hainguyentien203 <hainguyentien203@gmail.com>
2025-03-08 09:15:04 +0700

    Bai 2

--- nguyen_hai/main.c
+++ nguyen_hai/main.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 void main() {
-	float a;
-	printf("Nhap vao so diem trung binh cua cac hoc sinh a: ");
-	scanf_s("%f", &a);
-	if (a >= 8.0) {
-		printf("Hoc sinh nay xep loai gioi.\n");
+	int a, b, c;
+	printf("Nhap vao 3 so nguyen a, b, c: ");
+	scanf_s("%d %d %d", &a,&b,&c);
+	int max = a;
+	if (b > max) {
+		max = b;
 	}
-	else if (a >= 6.5) {
-		printf("Hoc sinh nay xep loai kha.\n");
+	if (c > max) {
+		max = c;
 	}
-	else if (a >= 5.0) {
-		printf("Hoc sinh nay xep loai trung binh.\n");
-	}
-	else {
-		printf("Hoc sinh nay xep loai yeu.\n");
-	}
+	 printf("Gia tri lon nhat cua 3 so nay la max = %d", max);
 }
