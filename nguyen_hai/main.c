a438486aa57a067802fc1888da4232684093176f
hainguyentien203 <hainguyentien203@gmail.com>
2025-03-08 10:03:38 +0700

    bai 3

--- nguyen_hai/main.c
+++ nguyen_hai/main.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 void main() {
-	int a, b, c;
-	printf("Nhap vao 3 so nguyen a, b, c: ");
-	scanf_s("%d %d %d", &a,&b,&c);
-	int max = a;
-	if (b > max) {
-		max = b;
+	float a, b, c,x1,x2,x;
+	float delta;
+	printf("Nhap gia tri a,b,c: ");
+	scanf_s("%f %f %f", &a,& b,& c);
+	delta = b * b - 4 * a * c;
+	if (a == 0) {
+		if (b != 0) {
+			x = -c / b;
+			printf("Phuong trinh la bac nhat va co nghiem la x = %f\n", x);
+		}
+		else printf("Phuong trinh vo nghiem.\n");
 	}
-	if (c > max) {
-		max = c;
+	else if (delta < 0) {
+		printf("Phuong trinh vo nghiem.\n");
 	}
-	 printf("Gia tri lon nhat cua 3 so nay la max = %d", max);
+	else if (delta == 0) {
+		x = x1 = x2 = -b / (2 * a);
+		printf("Phuong trinh co 1 nghiem kep la x= %f\n", x);
+	}
+	else {
+		x1 = (-b + sqrt(delta)) / (2 * a);
+		x2 = (-b - sqrt(delta)) / (2 * a);
+		printf("Phuong trinh co 2 nghiem phan biet la x1 = %f va x2 = %f\n", x1, x2);
+	}
 }
