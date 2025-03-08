d1a13f8a7cb7ff46d30524a2d828148cfd74d2be
hainguyentien203 <hainguyentien203@gmail.com>
2025-03-08 10:28:40 +0700

    Bai 7

--- nguyen_hai/main.c
+++ nguyen_hai/main.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 void main() {
-	int n;
-	printf("Nhap vao gia tri so nguyen n: ");
-	scanf_s("%d", &n);
-	if (n%2==0) {
-		printf("n la so Chan.\n");
+	int kwh;
+	float sotien;
+	printf("Nhap vao so KWh gia dinh su dung: ");
+	scanf_s("%d", &kwh);
+	while (kwh < 0) {
+		printf("Nhap lai so KWh gia dinh su dung: ");
+		scanf_s("%d", &kwh);
 	}
-	else printf("n la so Le.\n");
+	if (kwh <= 50) {
+		sotien = 1.678 * kwh;
+		printf("So tien ma gia dinh phai tra la: %f", sotien);
+	}
+	else if (kwh <= 100) {
+		sotien = 1.678 * 50+(kwh-50)*1.734;
+		printf("So tien ma gia dinh phai tra la: %f", sotien);
+	}
+	else if (kwh <= 200) {
+		sotien = 1.678 * 50 + 50 * 1.734 + (kwh-100)*2.014;
+		printf("So tien ma gia dinh phai tra la: %f", sotien);
+	}
+	else if (kwh <= 300) {
+		sotien = 1.678 * 50 + 50 * 1.734 + 100 * 2.014 + (kwh-200)*2.536;
+		printf("So tien ma gia dinh phai tra la: %f", sotien);
+	}
+	else if (kwh <= 400) {
+		sotien = 1.678 * 50 + 50 * 1.734 + 100 * 2.014 + 100 * 2.536 + (kwh - 300) * 2.834;
+		printf("So tien ma gia dinh phai tra la: %f", sotien);
+	}
+	else {
+		sotien = 1.678 * 50 + 50 * 1.734 + 100 * 2.014 + 100 * 2.536 + 100 * 2.834 + (kwh - 400) * 2.927;
+		printf("So tien ma gia dinh phai tra la: %f", sotien);
+	}
 }
