12064e61790dfd50d9ff9245405ea7e3496fcb95
hainguyentien203 <hainguyentien203@gmail.com>
2025-03-12 18:47:15 +0700

    bai 6 UCLN

--- nguyen_hai/main.c
+++ nguyen_hai/main.c
@@ -1,16 +1,22 @@
 ﻿#include <stdio.h>
 #include <math.h>
-void main(){
-	int n;
-	printf("Nhap vao mot so tu nhien n: ");
-	scanf_s("%d", &n);
-	for (int i = 1; i < n; i++) {
-		int nt = 0;
-		for (int j = 1; j <= i; j++) {
-			if (i % j == 0) nt++;
-		} if (nt == 2) {
-			printf("%d la so nguyen to\n", i);
-		}
+int min(int A, int B) {
+	int min = A;
+	if (B < min) min = B;
+	return min;
+}
+int UCLN(int A, int B) {
+	for (int i = min(A, B); i > 0; i--) {
+		if (A % i == 0 && B % i == 0)
+			return i;
 	}
+}
+void main(){
+	int A, B,c;
+	printf("Nhap gia tri cua so nguyen A: ");
+	scanf_s("%d", &A);
+	printf("Nhap gia tri cua so nguyen B: ");
+	scanf_s("%d", &B);
+	printf("UCLN giua %d va %d la : %d", A, B, UCLN (A,B));
 }
 
