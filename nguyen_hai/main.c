108c8caa7a24ea5b63207c5d0b5d1080a228a7d9
hainguyentien203 <hainguyentien203@gmail.com>
2025-03-12 18:00:50 +0700

    bai 5 so nguyen to

--- nguyen_hai/main.c
+++ nguyen_hai/main.c
@@ -2,15 +2,15 @@
 #include <math.h>
 void main(){
 	int n;
-	double tongtien= 0;
-	double tienlai=8.2/100;
-	double tiengui=0;
-	double nam=0;
-	printf("Nhap vao so tien gui ban dau: ");
-	scanf_s("%lf", &tiengui);
-	printf("Nhap so nam gui: ");
-	scanf_s("%lf", &nam);
-	tongtien = tiengui * pow(1 + tienlai, nam);
-	printf("Tong so tien nhan duoc khi gui %3f sau %3f nam la: %3f", tiengui, nam, tongtien);
+	printf("Nhap vao mot so tu nhien n: ");
+	scanf_s("%d", &n);
+	for (int i = 1; i < n; i++) {
+		int nt = 0;
+		for (int j = 1; j <= i; j++) {
+			if (i % j == 0) nt++;
+		} if (nt == 2) {
+			printf("%d la so nguyen to\n", i);
+		}
+	}
 }
 
