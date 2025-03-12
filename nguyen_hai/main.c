0e0f462436210ac06f4491c6ddb5b98454867f76
hainguyentien203 <hainguyentien203@gmail.com>
2025-03-12 19:28:58 +0700

    bai 8 Ct thue ca nhan

--- nguyen_hai/main.c
+++ nguyen_hai/main.c
@@ -1,16 +1,25 @@
 ﻿#include <stdio.h>
 #include <math.h>
 int main(){
-	int a, b;
-	double f_x=0;
-	printf("Nhap gia tri cua so nguyen a: ");
-	scanf_s("%d", &a);
-	printf("Nhap gia tri cua so nguyen b: ");
-	scanf_s("%d", &b);
-	double h = (double)fabs(a - b) / 1000;
-	for (int i = 0; i <= 1000; i++) {
-		f_x = f_x + (pow(a + i * h, 2) + pow(a + (1 + i) * h, 2)) * h / 2;
+	int thu_nhap = 0;
+	float thue = 0;
+	printf("Nhap vao thu nhap hang thang: ");
+	scanf_s("%d", &thu_nhap);
+	if (thu_nhap <= 5000) {
+		thue = thu_nhap * 0.05;
 	}
-	printf("Gia tri cua tich phan f(x)=x^2 voi tan so mau 1000 lan la : %3f", f_x);
+	else if (thu_nhap <= 10000) {
+		thue = 5000 * 0.05 + (thu_nhap-5000)*0.1;
+	}
+	else if (thu_nhap <= 18000) {
+		thue = 5000 * 0.05 + 5000 * 0.1 + (thu_nhap-10000)*0.15;
+	}
+	else if (thu_nhap <= 32000) {
+		thue = 5000 * 0.05 + 5000 * 0.1 + 8000 * 0.15 + (thu_nhap-18000)*0.2;
+	}
+	else if (thu_nhap <= 52000) {
+		thue = 5000 * 0.05 + 5000 * 0.1 + 8000 * 0.15 + 14 * 0.2 + (thu_nhap-32000)*0.25;
+	} else thue = 5000 * 0.05 + 5000 * 0.1 + 8000 * 0.15 + 14 * 0.2 + 20 * 0.25 + (thu_nhap-52000)*0.35;
+	printf("Thue phai dong theo thu nhap %d la: %3f", thu_nhap, thue);
 }
 
