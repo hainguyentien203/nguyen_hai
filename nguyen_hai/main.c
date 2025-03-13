51a859cb840c040811561d0c6b80d9ba2d8236b0
hainguyentien203 <hainguyentien203@gmail.com>
2025-03-13 19:57:22 +0700

    mang

--- nguyen_hai/main.c
+++ nguyen_hai/main.c
@@ -1,25 +1,19 @@
 ﻿#include <stdio.h>
 #include <math.h>
-int main(){
-	int thu_nhap = 0;
-	float thue = 0;
-	printf("Nhap vao thu nhap hang thang: ");
-	scanf_s("%d", &thu_nhap);
-	if (thu_nhap <= 5000) {
-		thue = thu_nhap * 0.05;
+#include <conio.h>
+void main() {
+	int a[10] = { 0 };
+	int tong = 0;
+	for (int i = 0; i < 10; i++) {
+		printf("a[%d] = ", i);
+		scanf_s("%d", &a[i]);
 	}
-	else if (thu_nhap <= 10000) {
-		thue = 5000 * 0.05 + (thu_nhap-5000)*0.1;
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", a[i]);
 	}
-	else if (thu_nhap <= 18000) {
-		thue = 5000 * 0.05 + 5000 * 0.1 + (thu_nhap-10000)*0.15;
+	for (int i = 0; i < 10; i++) {
+		tong = tong + a[i];
 	}
-	else if (thu_nhap <= 32000) {
-		thue = 5000 * 0.05 + 5000 * 0.1 + 8000 * 0.15 + (thu_nhap-18000)*0.2;
-	}
-	else if (thu_nhap <= 52000) {
-		thue = 5000 * 0.05 + 5000 * 0.1 + 8000 * 0.15 + 14 * 0.2 + (thu_nhap-32000)*0.25;
-	} else thue = 5000 * 0.05 + 5000 * 0.1 + 8000 * 0.15 + 14 * 0.2 + 20 * 0.25 + (thu_nhap-52000)*0.35;
-	printf("Thue phai dong theo thu nhap %d la: %3f", thu_nhap, thue);
+	printf("tong = %d", tong);
 }
 
