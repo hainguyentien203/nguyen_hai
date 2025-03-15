e58fa1326c41061e3ed51d0e88aada0dce1da469
hainguyentien203 <hainguyentien203@gmail.com>
2025-03-15 12:05:51 +0700

    Bai 4 thay the pt am = 0

--- nguyen_hai/main.c
+++ nguyen_hai/main.c
@@ -1,30 +1,39 @@
 ﻿#include <stdio.h>
 #include <conio.h>
 #include <math.h>
-
-void main()
-{
-	int a[5] = { 0 };
-	printf("Nhap cac phan tu cua mang: \n");
-	for (int i = 0; i < 5; i++) {
+void nhap(int a[], int n) {
+	for (int i = 0; i < n; i++) {
 		printf("a[%d] = ", i);
 		scanf_s("%d", &a[i]);
 	}
-	printf("Mang vua nhap la: \n");
-	for (int i = 0; i < 5; i++) {
-		printf(" a[%d] = %d ",i, a[i]);
+}
+void xuat(int a[], int n) {
+	for (int i = 0; i < n; i++) {
+		printf(" a[%d] = %d ", i, a[i]);
 	}
-	int dem = 0;
-	int max = a[0];
 	printf("\n");
-	for (int i = 0; i < 5; i++) {
-		if (a[i] >= max) {
-			max = a[i];
+}
+void thay_am (int a[], int n) {
+	for (int i = 0; i < n; i++) {
+		if (a[i] < 0) {
+			a[i] = 0;
 		}
 	}
-	for (int i = 0; i < 5; i++) {
-		if (a[i]==max)
-		printf("So lon nhat trong mang la %d, dung tai vi tri: %d\n",max, i);
-	}
 }
+void main()
+{
+	int a[30] = { 0 };
+	int n = 0;
+	do {
+		printf("Nhap so phan tu cua mang : n = ");
+		scanf_s("%d", &n);
+	} while (n <= 0 || n >= 30);
+	nhap(a, n);
+	printf("Mang vua nhap la: \n");
+	xuat(a, n);
+	thay_am(a, n);
+	printf("Mang sau khi thay the phan tu am bang 0 la: \n");
+	xuat(a, n);
+}
+
 
