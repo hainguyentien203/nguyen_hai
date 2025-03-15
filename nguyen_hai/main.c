49ec15e43f3c04608150fe068819e03d1f76ae9d
hainguyentien203 <hainguyentien203@gmail.com>
2025-03-15 13:06:52 +0700

    Bai 8 Hoan vi 2 so nguyen

--- nguyen_hai/main.c
+++ nguyen_hai/main.c
@@ -13,14 +13,10 @@ void xuat(int a[], int n) {
 	}
 	printf("\n");
 }
-void max (int a[], int n) {
-	int max = a[0];
-	for (int i = 0; i < n; i++) {
-		if (a[i] > max) {
-			max = a[i];
-		}
-	}
-	printf("Gia tri lon nhat trong mang la: %d", max);
+void swap (int a[], int i, int j) {
+	int tg = a[i];
+	a[i] = a[j];
+	a[j] = tg;
 }
 void main()
 {
@@ -33,7 +29,16 @@ void main()
 	nhap(a, n);
 	printf("Mang vua nhap la: \n");
 	xuat(a, n);
-	max(a, n);
+	int vt1 = 0;
+	int vt2 = 0;
+	do {
+		printf("Nhap vi tri 2 so nguyen muon hoan vi: ");
+		scanf_s("%d", &vt1);
+		scanf_s("%d", &vt2);
+	} while (vt1 < 0 && vt1 >= n && vt2 < 0 && vt2 >= n);
+	swap(a, vt1, vt2);
+	printf("Mang sau khi hoan vi la: ");
+	xuat(a, n);
 	printf("\n");
 }
 
