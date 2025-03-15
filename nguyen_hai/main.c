4a5e0374ad366274b32832c5a7751215bc017902
hainguyentien203 <hainguyentien203@gmail.com>
2025-03-15 16:49:46 +0700

    string bai 4 dem so tu

--- nguyen_hai/main.c
+++ nguyen_hai/main.c
@@ -11,19 +11,18 @@ void nhap(char S[100]) {
 void xuat(char S[100]) {
 	printf("Chuoi vua nhap la: %s\n",S);
 }
-void inhoa(char S[100]) {
+void dem (char S[100]) {
+	int d = 0;
 	for (int i = 0; i < strlen(S); i++) {
-		if (S[i] >= 'a' && S[i] <= 'z'); {
-			S[i] = toupper(S[i]);
-		}
+		if (S[i] != ' ' && (S[i + 1] == ' ' || S[i + 1] == '\0'))
+			d++;
 	}
+	printf("chuoi co %d ki tu.", d);
 }
 void main() {
 	char S[100] = { 0 };
 	nhap(S);
 	xuat(S);
-	inhoa(S);
-	printf("Chuoi sau khi thay the chu thuong thanh chu in hoa la :\n");
-	xuat(S);
+	dem(S);
 }
 
