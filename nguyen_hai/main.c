cc619c3fe47ba6f0860da32e25742890661ff3d1
hainguyentien203 <hainguyentien203@gmail.com>
2025-03-18 17:16:15 +0700

    String bai 7

--- nguyen_hai/main.c
+++ nguyen_hai/main.c
@@ -1,33 +1,36 @@
 ﻿#include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-void nhap(char S[100]) {
+char* nhap(char S[]) {
 	do {
 		printf("Nhap vao mot chuoi ky tu (khac rong): ");
 		fflush(stdin);
 		gets(S);
 	} while (strcmp(S,"") == 0);
 }
-void xuat(char S[100]) {
+char* xuat(char S[]) {
 	printf("%s\n",S);
 }
-void tim_c(char S[100]) {
+char* tim_chuoi(char S[], char S_con[]) {
 	int dem = 0;
 	for (int i = 0; i < strlen(S); i++) {
-		if (S[i] == 'c') {
+	for (int j = 0; j < strlen(S_con); j++) {
+		if (S[i + j] == S_con[j] && S[i+strlen(S_con)-1] == S_con [strlen(S_con)-1]) {
+			printf("Chuoi con duoc tim thay tai vi tri %ld (dia chi: %p)\n",i, &S[i]);
 			dem++;
-			printf("ky tu c trong chuoi duoc tim thay tai vi tri %ld (dia chi: %p)\n", i, &S[i]);
+			}
 		}
-	} 
-	if (dem == 0){
-		printf("Khong co ky tu c nao duoc tim thay trong chuoi.");
-	}
+	} if (dem==0) printf("Khong tim thay chuoi con trong chuoi chinh.\n");
 }
 void main() {
 	char S[100] = { 0 };
+	char S_con[100] = { 0 };
 	nhap(S);
 	printf("Chuoi vua nhap la: ");
 	xuat(S);
-	tim_c(S);
+	nhap(S_con);
+	printf("Chuoi con vua nhap la: ");
+	xuat(S_con);
+	tim_chuoi(S,S_con);
 }
 
