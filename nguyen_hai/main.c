336264d85bc21ae150ed00bc90a610f1294558dd
hainguyentien203 <hainguyentien203@gmail.com>
2025-03-25 20:52:47 +0700

    mang tinh diem trung binh va xep loai

--- nguyen_hai/main.c
+++ nguyen_hai/main.c
@@ -1,26 +1,30 @@
 ﻿#include <stdio.h>
 #include <string.h>
 
-struct ps {
-    int x;
-    int y;
-};
+typedef struct {
+    char* ten;
+    int tuoi;
+    float diem_toan;
+    float diem_van;
+    float diem_tb;
+    float xep_loai;
+} hs_t;
+hs_t tinh_toan(hs_t* mang, int sl) {
+    float diem_tb = ((float)diem_toan + (float)diem_van) / 2;
+    float xep_loai = 0;
+    for (int i = 0; i < sl; i++) {
+        if (mang[i].diem_tb >= 8.0) { mang[i].xep_loai = 0; }
+        if (mang[i].diem_tb >=6.5) { mang[i].xep_loai = 1; }
+        if (mang[i].diem_tb >= 5) { mang[i].xep_loai = 2; }
+        else mang[i].xep_loai = 3;
+    }
 
-struct ps nhan_ps(struct ps A, struct ps B) {
-    struct ps kq;
-    kq.x = A.x * B.x;
-    kq.y = A.y * B.y;
-    return kq;
 }
-
 void main() {
-    struct ps A;
-    struct ps B;
-    A.x = 1;
-    A.y = 2;
-    B.x = 3;
-    B.y = 4;
-
-    struct ps C = nhan_ps(A, B);
-    printf("Ket qua la: %d/%d\n", C.x, C.y);
+    hs_t mang_hs[]{
+        {"Nguyen van A",18,8.0,6.5},
+        {"Nguyen van B",18,7.0,6.5},
+        {"Nguyen van C",18,8.9,8.0},
+        {"Nguyen van D",18,8.0,9.5},
+    };
 }
