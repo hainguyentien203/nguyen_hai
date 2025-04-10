45a7e5a92c0ab292c9caaae7f0c1e3bf6886177b
hainguyentien203 <hainguyentien203@gmail.com>
2025-04-10 20:43:27 +0700

    tao thread

--- nguyen_hai/main.c
+++ nguyen_hai/main.c
@@ -1,24 +1,28 @@
 ﻿#include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-typedef union {
-	unsigned short x;
-	unsigned char byte[2];
-	/*struct {
-		unsigned char low;
-		unsigned char high;
-	};*/
-}union_t;
-
+#include <Windows.h>
+DWORD WINAPI Thread_1(_In_ LPVOID lpParameter) {
+	while (1) {
+		printf("Thread 1 is running ...\n");
+		Sleep(1000);
+	}
+}
+DWORD WINAPI Thread_2(_In_ LPVOID lpParameter) {
+	while (1) {
+		printf("Thread 2 is running ...\n");
+		Sleep(1000);
+	}
+}
 void main() {
-	unsigned short value = 0x1234;
-	unsigned char high;
-	unsigned char low;
-	union_t A;
-	A.x = value;
-	low = A.byte[0];
-	high = A.byte[1];
-	printf("Gia tri A = 0x%X\n", A.x);
-	printf("Byte low = 0x%X\n", low);
-	printf("Byte high = 0x%X\n", high);
+	HANDLE thread_1 = CreateThread(NULL, 1024, Thread_1, NULL, 0, NULL);
+	HANDLE thread_2 = CreateThread(NULL, 1024, Thread_2, NULL, 0, NULL);
+	int count = 0;
+	while (1) {
+		printf("main is running ...\n");
+		Sleep(1000);
+		if (count++ == 5) {
+			printf("suspend thread 1: %d\n", SuspendThread(Thread_1));
+			printf("suspend thread 1: %d\n", SuspendThread(Thread_1));
+		}
+	}
 }
