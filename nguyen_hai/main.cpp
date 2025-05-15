f8cded9f70c667e39efcb5fe0c6b3337bc3a4791
hainguyentien203 <hainguyentien203@gmail.com>
2025-05-15 14:55:54 +0700

    class mang

--- nguyen_hai/main.cpp
+++ nguyen_hai/main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <malloc.h>
+#include <iostream>
 class mang {
 public:
 	int* data;
@@ -13,14 +14,33 @@ public:
 	int& operator[](int index) {
 		return data[index];
 	}
-	int operator[](int index) const {
+	/*int operator[](int index) const {
 		return data[index];
+	}*/
+	// viet method them mot doi tuong vao trong mang o vi tri cuoi cung
+	void push_back(int val) {
+		// cap phat vung nho moi lon hon vung nho cu
+		int* newtemp = (int*)malloc((size + 1) * sizeof(int));
+		if (newtemp == NULL) {
+			throw std::runtime_error("Khong the malloc\n");
+		}
+		// copy du lieu tu vung nho cu sang vung nho moi va set gia tri cua doi tuong 
+		for (int i = 0; i < size; i++) {
+			newtemp[i] = data[i];
+		}
+		newtemp[size] = val;
+		// huy vung nho cu
+		free(data);
+		// cap nhat dia chi cua vung nho moi vao con tro data
+		data = newtemp;
+		// cap nhat lai gia tri cua bien size
+		size++;
 	}
 	~mang() {
 		free(data);
 	}
 };
-int tong(const mang &x) {
+int tong( mang &x) {
 	int sum = 0;
 	for (int i = 0; i < x.size; i++) {
 		sum = sum + x[i];
@@ -32,6 +52,7 @@ int main() {
 	arr[0] = 1;
 	arr[1] = 4;
 	arr[2] = 2;
+	arr.push_back(5);
 	int x = tong(arr);
 	printf("Tong mang x = %d\n", x);
 	return 0;
